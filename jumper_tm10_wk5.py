979eda6b0738b299ed3ad4aebad357c3a200ac42
jersbluesky <joh02052@byui.edu>
2022-05-19 21:31:12 -0600

    Pseudo-code vs 1

--- jumper_tm10_wk5.py
+++ jumper_tm10_wk5.py
@@ -1,1 +1,60 @@
+# Jumper Game (Generic Brand of 'Hangman')
+# Team 10 
+# 5/19/2022
+
+import random
+
+# The puzzle is a secret word randomly chosen from a list.
+# # Words Class
+# # init
+# # # Attributes
+# # # # self.words -> array 
+# # # # self.puzzle_word -> randomly chosen word
+# # # Methods
+# # # # set_word(self)
+# # # # # logic (random.xyz)
+
+# # Letters Class
+# # init
+# # # Attibutes
+# # # # self.letters -> array
+# # Methods
+# # # # remove_letters(self)
+# # # # # logic: Removes letters as the user chooses them
+
+# # Rules Class
+# # init
+# # # Attributes:
+# # # # self.user_choice -> taken from input
+# # # # self.jumper_image -> start with initial ascii
+# # # # self.num_wrong -> 1-5, start at 0
+# # # # self.winner -> boolean
+# # # super init
+# # Methods
+# # # If the guess is correct, the letter is revealed.
+# # # If the guess is incorrect, a line is cut on the player's parachute.
+# # # if num_wrong eq 1
+# # # # jumper_image eq 
+# # # elif num_wrong eq 2-5
+# # # # jumper_image eq ..
+# # # elif the player has no more parachute the game is over.
+# # # # If the puzzle is solved the game is over.
+
+
+# # Display Class
+# # # Displays/updates the ASCII image
+
+# Classes
+# # Attribute (variables)
+# # Methods (functions)
+
+
+
+# main
+# Game rules
+# create the objects
+# # word array
+# # alphabet array
+# User input
+# # 
 
