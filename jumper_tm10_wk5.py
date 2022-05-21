4820d61915f6c5e6a973c6ea57e325be19a9d3d6
Merrik115 <caleb.lubits@gmail.com>
2022-05-21 16:53:19 -0600

    Added comments to the display class.

--- jumper_tm10_wk5.py
+++ jumper_tm10_wk5.py
@@ -46,29 +46,36 @@ import random
 
 # CALEB
 # # Display Class
-# # # Displays/updates the ASCII image
+# # # Displays the parachute and man. Requires the number of mistakes to know how much of the parachute to get rid of.
 class Display:
+
     def __init__(self):
+        #Seperatted each step into a picture which will disappear when there is a mistake.
         self.first_mistake = "  ___"
         self.second_mistake = " /___\ "
+        #The third mistake is actually just the second line without the / and \.
         self.third_mistake = "  ___"
         self.fourth_mistake = " \   /"
         self.fifth_mistake = "  \ /"
         self.guy = "   o\n  /|\ \n  / \ "
+        #Dead guy is used at the very end to represent a loss.
         self.dead_guy = "   x\n  /|\ \n  / \ "
+        #Decided to combine all of the different pictures into an array so I could print it easier.
         self.whole_picture = [self.first_mistake, self.second_mistake, self.fourth_mistake, self.fifth_mistake, self.guy]
-
+        
+    #display function uses if statements to change the array and then displays it 
     def display(self, num_wrong):
         if num_wrong >= 1:
-            self.whole_picture[0] = " "
+            self.whole_picture[0] = ""
         if num_wrong >= 2:
+            #replaces second_mistake with third_mistake
             self.whole_picture[1] = self.third_mistake
         if num_wrong >= 3:
-            self.whole_picture[1] = " "
+            self.whole_picture[1] = ""
         if num_wrong >= 4:
-            self.whole_picture[2] = " "
+            self.whole_picture[2] = ""
         if num_wrong >= 5:
-            self.whole_picture[3] = " "
+            self.whole_picture[3] = ""
             self.whole_picture[4] = self.dead_guy
         print(*self.whole_picture, sep='\n')
 
