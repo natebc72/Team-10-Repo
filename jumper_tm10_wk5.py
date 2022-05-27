2b312622b91c7fcf61936c9b4b11ac7d58f5a175
jersbluesky <joh02052@byui.edu>
2022-05-27 01:40:40 -0600

    This is Jeremy's final edit, functional code.

--- jumper_tm10_wk5.py
+++ jumper_tm10_wk5.py
@@ -1,72 +1,125 @@
 # Jumper Game (Generic Brand of 'Hangman')
 # Team 10 
-# 5/19/2022
+# 5/27/2022
 
 import random
+from numpy import number
 
 # J'DEE
 # The puzzle is a secret word randomly chosen from a list.
-class words:
+# # Words Class
+class Words:
     """ The responsibility of this class is to generate and randomly pick from a list of words.
     
     Attributes: words: an array of words to choose from
                 puzzle_word: the chosen word for the game
     """
-def __init__(self):
-    self.words = ["books", "class", "games", "plant", "water"]
-    self.puzzle_word = ""
+    def __init__(self):
+        self.puzzle_word = ""
 # # # Methods
 # # # # set_word(self)
-def set_word(self):
-    """generates the puzzle word from the words array, and sets the value of the puzzle word to the selected word.
+    def set_word(self):
+        """generates the puzzle word from the words array, and sets the value of the puzzle word to the selected word.
     
-    Arguments: self: an insance of words.
-    """
-    self.puzzle_word = random.self.words()
-# # # # # logic (random.xyz)
+    Arguments: self: an insance of words."""
+        words = ["bolts", "claps", "games", "plant", "water"]
+        self.puzzle_word = random.choice(words)
+        return self.puzzle_word
+    
+        
 
+
+    
 # NATE
-# # Letters Class
-"""""variable will hold the gueesed letters"""
-lettersGuessed = ""
-"""logic: Removes letters as the user chooses them and stores them into the variable."""
-"""guess is a placeholder for the user input."""
-lettersGuessed = lettersGuessed + guess(userinput)
-
-
-# JEREMY
-# # Rules Class
+# Letters Class
+class Letters():
 # # init
-# # # Attributes:
-# # # # self.user_choice -> taken from input
-# # # # self.jumper_image -> start with initial ascii
-# # # # self.num_wrong -> 1-5, start at 0
-# # # # self.winner -> boolean
-# # # super init
-# # Methods
-# # # rules(self)
-# # # # If the guess is correct, the letter is revealed.
-# # # # If the guess is incorrect, a line is cut on the player's parachute.
-# # # # if num_wrong eq 1
-# # # # # jumper_image eq 
-# # # # If the puzzle is solved the game is over.
-
-# CALEB
-# # Display Class
-# # # Displays/updates the ASCII image
-# _ _ _ _ _
-#
-#  
-
-
-   
-
-# # # Displays the parachute and man. Requires the number of mistakes to know how much of the parachute to get rid of.
-class TerminalService:
-
-
     def __init__(self):
-        #Seperatted each step into a picture which will disappear when there is a mistake.
+# # # Attibutes
+        self.letters_array = []
+        self.user_choice = ""
+        self.already_guessed = False
+
+# Methods
+# # # remove_letters(self)
+
+    def add_letters(self):
+        self.user_choice = input("Choose a letter: ")
+        for i in self.letters_array:
+            if i == self.user_choice:
+                print("You already guessed this. Try again.")
+                self.already_guessed = True
+                return self.already_guessed
+            else:
+                self.already_guessed = False
+        self.letters_array.append(self.user_choice)
+        return self.already_guessed
+
+# # JEREMY
+# # # Game Class
+class Game():
+# # init
+    def __init__(self):
+        self.num_wrong = 0
+        self.winner = False
+        self.answer_array = [" _ ", " _ ", " _ ", " _ ", " _ "]
+    
+    """This is the main section of game-play, in which the logic of all of the other classes come together."""
+    def play_round(self):
+        """Objects are created within the method of this class based on previous classes."""
+        game_letter = Letters()
+        game_word = Words()
+        term_serv = TerminalService()
+        letters = ""
+        """term_serv is the object created from the TerminalService class, and display is the method within that
+        class."""
+        term_serv.display()
+        puzzle_array = game_word.set_word()
+        """A for loop is here to call the letters array from the Letter class"""
+        for i in game_letter.letters_array:
+            letters += i
+        """Here we have the main logic game loop, keeping the game rolling. It tests if the game is won or lost,
+        and loops through the logical rules."""
+        while term_serv.num_wrong != 5:
+            while self.winner != True:
+                game_letter.add_letters()
+                for i in puzzle_array:
+                    if (game_letter.user_choice == i):
+                        print("Super Duper!")
+                        place = puzzle_array.index(i)
+                        self.answer_array[place] = i
+                        right_answer = True
+                        break
+                    else:
+                        right_answer = False
+                if game_letter.already_guessed == False:
+                    letters += game_letter.user_choice
+                if right_answer == False:
+                    term_serv.num_wrong += 1
+                    term_serv.remove_chute()                
+                print(" ")
+                print(game_play.answer_array[0], game_play.answer_array[1], game_play.answer_array[2],game_play.answer_array[3],game_play.answer_array[4])
+                print(" ")
+                print(f'\nNumber of wrong guesses: {term_serv.num_wrong}\n')
+                term_serv.display()
+                print(" ")
+                print(f'letters you have guessed: {letters}\n')
+                print("")
+                if self.answer_array.count(" _ ") == 0:
+                    print("You have won!")
+                    self.winner == True
+                    return self.winner
+                if term_serv.num_wrong >= 5:
+                    print("That's too many wrong. I'm sorry, your jumper's parachute stopped functioning, and now they are dead.")
+                    break
+            print("Dead, dead, dead.")
+        
+
+
+
+class TerminalService:
+    def __init__(self):
+        #Separated each step into a picture which will disappear when there is a mistake.
         self._first_mistake = "  ___"
         self._second_mistake = " /___\ "
         #The third mistake is actually just the second line without the / and \.
@@ -81,39 +134,27 @@ class TerminalService:
         self.num_wrong = 0
         
     #display function uses if statements to change the array and then displays it 
-    def display(self):
-        if self.num_wrong >= 1:
-            self._whole_picture.remove("  ___")
-        if self.num_wrong >= 2:
+    def remove_chute(self):
+        if (self.num_wrong == 1) | (self.num_wrong == 3) | (self.num_wrong == 4):
+            self._whole_picture.remove(self._whole_picture[0])
+        elif self.num_wrong == 2:
             #replaces second_mistake with third_mistake
             self._whole_picture[0] = self._third_mistake
-        if self.num_wrong >= 3:
-            self._whole_picture.remove("  ___")
-        if self.num_wrong >= 4:
-            self._whole_picture.remove(" \   /")
-        if self.num_wrong >= 5:
-            self._whole_picture.remove("  \ /")
+        elif self.num_wrong == 5:
+            self._whole_picture.remove(self._whole_picture[0])
             self._whole_picture[0] = self._dead_guy
+        elif self.num_wrong <=0:
+            self._whole_picture = self._whole_picture
+        return self._whole_picture
+
+    def display(self):
         print(*self._whole_picture, sep='\n')
 
-# MARCOS
-# main
-# Game rules
-# create the objects
-# # word array
-# # alphabet array
-# User input
-# # 
+        
+game_play = Game()
+game_play.play_round()
 
-#def main():
-    #words = Words()
-    #letters = Letters()
-    #rules = Rules()
-    #display = Display()
 
-    #User Input
-    #rules.user_choice = input("Guess a letter [a-z]: ")
-    #rules.verify_letter(words.puzzle_word)
-    
+
 
 
