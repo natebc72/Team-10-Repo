b447ede1abb7e9d4d60cad75aa5afd9739917e1c
jdharri47 <har20040@byui.edu>
2022-07-15 12:06:41 -0600

    did fire_bullet method

--- Final-Project/tank.py
+++ Final-Project/tank.py
@@ -13,3 +13,19 @@ class Tank(Actor):
     def __init__(self, player):
         super().__init__()
         self.player = player
+
+    def fire_bullet(self):
+        player = self._player
+    
+        bullet = Actor()
+
+        velocity = bullet.get_velocity()
+        offset = velocity.reverse()
+        position = bullet.get_position().add(offset)
+        bullet.set_position(position)
+        bullet.set_velocity(velocity)
+        bullet.set_text("o")
+        if self.player == 1:
+            bullet.set_color(constants.BLUE)
+        if self.player == 2:
+            bullet.set_color(constants.RED)      
