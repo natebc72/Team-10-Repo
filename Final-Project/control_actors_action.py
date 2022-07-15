3b39cb373b4e9557b2ce9e0e2084101efaf412b7
natebc72 <nbchristiansen72@gmail.com>
2022-07-14 20:45:28 -0700

    updates

--- Final-Project/control_actors_action.py
+++ Final-Project/control_actors_action.py
@@ -1,14 +1,14 @@
 import constants
 from action import Action
 from point import Point
-from snake import Snake
+from tank import Tank
 
 
 class ControlActorsAction(Action):
     """
-    An input action that controls the snake.
+    An input action that controls the tank.
     
-    The responsibility of ControlActorsAction is to get the direction and move the snake's head.
+    The responsibility of ControlActorsAction is to get the direction and move the tanks.
 
     Attributes:
         _keyboard_service (KeyboardService): An instance of KeyboardService.
@@ -31,40 +31,30 @@ class ControlActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        # left
-        if self._keyboard_service.is_key_down('a'):
-            self._direction_one = Point(-constants.CELL_SIZE, 0)
-        
-        # right
-        if self._keyboard_service.is_key_down('d'):
-            self._direction_one = Point(constants.CELL_SIZE, 0)
-        
-        # up
+        # first tank up
         if self._keyboard_service.is_key_down('w'):
             self._direction_one = Point(0, -constants.CELL_SIZE)
         
-        # down
+        # first tank down
         if self._keyboard_service.is_key_down('s'):
             self._direction_one = Point(0, constants.CELL_SIZE)
-        
-        # left
-        if self._keyboard_service.is_key_down('j'):
-            self._direction_two = Point(-constants.CELL_SIZE, 0)
-        
-        # right
-        if self._keyboard_service.is_key_down('l'):
-            self._direction_two = Point(constants.CELL_SIZE, 0)
-        
-        # up
-        if self._keyboard_service.is_key_down('i'):
+            
+        #frist tank fire
+        if self._keyboard_service.is_key_down('f'):
+            
+          
+        # second tank up
+        if self._keyboard_service.is_key_down('u'):
             self._direction_two = Point(0, -constants.CELL_SIZE)
         
-        # down
-        if self._keyboard_service.is_key_down('k'):
+        # second tank down
+        if self._keyboard_service.is_key_down('j'):
             self._direction_two = Point(0, constants.CELL_SIZE)
+            
+        # second tank fire
+        if self._keyboard_service.is_key_down('l'):
 
-        snake_one = cast.get_first_actor("snake1")
-        snake_one.turn_head(self._direction_one)
-
-        snake_two = cast.get_first_actor("snake2")
-        snake_two.turn_head(self._direction_two)
+        tank_one = cast.get_first_actor("tank1")
+       
+        tank_two = cast.get_first_actor("tank2")
+        
