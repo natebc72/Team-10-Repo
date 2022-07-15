d5165439024e8d27c3eedc63f0aad3918388875f
marcosbarrozo <104003734+marcosbarrozo@users.noreply.github.com>
2022-07-15 10:27:01 -0600

    Update tank.py

--- Final-Project/tank.py
+++ Final-Project/tank.py
@@ -9,10 +9,36 @@ class Tank(Actor):
       P
     |/\|
     |\/|
-
-    Attributes:
-        _points (int): The number of points the food is worth.
     """
     def __init__(self, player):
         super().__init__()
         self.player = player
+        self._body = []
+        
+    def get_body():
+        return self._body
+        
+    def _prepare_body(self):
+        if self.player == 1:
+            x = int(constants.MAX_X / 7)
+            y = int(constants.MAX_Y / 2)
+            text = "==-"
+        if self.player == 2:
+            x = int(constants.MAX_X / 1.175)
+            y = int(constants.MAX_Y / 2)
+            text = "-=="
+
+        position = Point(x - 1 * constants.CELL_SIZE, y)
+        #velocity = Point(1 * constants.CELL_SIZE, 0)
+        
+        if self.player == 1:
+            color = constants.BLUE 
+        if self.player == 2
+            color = constants.RED
+            
+        tank = Actor()
+        tank.set_position(position)
+        #tank.set_velocity(velocity)
+        tank.set_text(text)
+        tank.set_color(color)
+        self._body.append(tank)
